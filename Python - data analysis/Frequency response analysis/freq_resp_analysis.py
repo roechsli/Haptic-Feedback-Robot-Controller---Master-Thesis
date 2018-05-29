fe30913c04241ac5bd3adeb71aab2560f533b46f
Oechslin Roman <oechslin@winter.ski>
2018-05-29 11:08:23 +0900

    minor changes in analysis file

--- Python - data analysis/Frequency response analysis/freq_resp_analysis.py
+++ Python - data analysis/Frequency response analysis/freq_resp_analysis.py
@@ -21,34 +21,42 @@ for file in os.listdir(directory):
 
     if ".csv" not in file:
         continue
-    frequency = float (get_freq_from_filename(file))
+    frequency = get_freq_from_filename(file)
+    frequency_float = float(frequency)
 
     # to restrict to first file only
-    if "f1.25_csv" not in file:
+    if "f12.6_csv" not in file:
         continue # write pass if all files should be treated
 
     df = pd.read_csv(directory + file, delimiter=';')
     #print(df.iloc[:, 3])
-
+    #print(df.iloc[:10,1])
+    #print(df.iloc[:10,3])
+    # make timeline continuous and not overflow and change from [us] to [s]
     for i in range(len(df.iloc[:,3]) -1):
         if df.iloc[i,3] > df.iloc[i+1,3]:
-            df.iloc[i:,3] = df.iloc[i:,3] + INTMAX
-        print(df.iloc[i,3])
+            df.iloc[i+1:,3] = df.iloc[i+1:,3] + INTMAX
+    for i in range(len(df.iloc[:, 3])):
+        df.iloc[i, 3] = df.iloc[i, 3] / 1000000 # convert to seconds
+        #print(df.iloc[i,3])
 
 
-    plt.plot(df.iloc[:, 3],df.iloc[:, 1])
-    #fig = plt.figure()
-    #fig.suptitle(titles[i] + ', Frequency of ' + frequency + ' Hz')
-    #plt.xlabel('Time [s]')
-    #plt.ylabel(ylabel[i])
+    fig = plt.figure()
+    plt.plot(df.iloc[:, 3],df.iloc[:, 0]) #TODO change this to 0
+    fig.suptitle('Distance reference, Frequency of ' + frequency + ' Hz')
+    plt.xlabel('Time [s]')
+    plt.ylabel('Distance [-]')
     #fig.savefig('figs/f' + frequency + '/plot_' + titles[i] + '.jpg')
     #plt.close(fig)
-    plt.show()
 
 
 
     #save left and right sensor data, plot and zoom
-
+    fig = plt.figure()
+    plt.plot(df.iloc[:, 3], df.iloc[:, 1])
+    fig.suptitle('Sensor readings left, Frequency of ' + frequency + ' Hz')
+    plt.xlabel('Time [s]')
+    plt.ylabel('Distance [-]')
     #calculate input magnitude
 
     #calculate left sensor magnitude and phase offset
@@ -58,3 +66,5 @@ for file in os.listdir(directory):
 #plot Bode diagram (freq_vec, magn_vec) and (freq_vec, phase_offset_vec)
 #save figures
 
+
+plt.show()
