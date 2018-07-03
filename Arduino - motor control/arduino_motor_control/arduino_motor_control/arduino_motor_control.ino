24aaa108fb717d7afcdd5df59bf9ff79fe1fcec2
Oechslin Roman <oechslin@winter.ski>
2018-07-03 19:47:27 +0900

    test feedback on real controller coupled with robot

--- Arduino - motor control/arduino_motor_control/arduino_motor_control/arduino_motor_control.ino
+++ Arduino - motor control/arduino_motor_control/arduino_motor_control/arduino_motor_control.ino
@@ -1,7 +1,7 @@
 /*
   Roman Oechslin
   Robot controller with haptic feedback
-  2018/04/06  Tokyo
+  2018/07/03  Tokyo
 */
 #define FASTADC 1
 
@@ -20,6 +20,7 @@
 int motorPinLowGain = 3;    // Motor connected to digital pin 3 (PWM)
 int motorPinHighGain = 11;    // Motor connected to digital pin 11 (PWM)
 int testPin = 13; // Used for testing the speed of Arduino
+int controlPin = 6; // Used for checking the distance
 
 int dist_ref = 0;
 float FILTER_CST = 0.5;
@@ -41,11 +42,16 @@ float numerator_r_last_f = 0;
 float dT = 0.001;
 float filter_coeff = 0.05;
 
+int photo_value_left_raw = 0;
+int photo_value_right_raw = 0;
+
 const int PHOTO_MIN_LEFT = 640;//650;
 const int PHOTO_MAX_LEFT = 840;//830;
 const int PHOTO_MIN_RIGHT = 700;//700;
 const int PHOTO_MAX_RIGHT = 880;//870;
 const int MAX_DISPLACEMENT_UM = 1800; // [um], has been measured
+
+const int MIN_DIST_THRESH = 10;
 
 unsigned long TIME_BEGIN = 0;
 unsigned long TIME_NOW = 0;
@@ -62,6 +68,7 @@ void setup() {
   pinMode(joystick_right_pin, INPUT);
   pinMode(photo_right_pin, INPUT);
   pinMode(testPin, OUTPUT);
+  pinMode(controlPin, OUTPUT);
 
   TCCR2B = TCCR2B & B11111000 | B00000001; // for PWM frequency of 31372.55 Hz
   #if FASTADC
@@ -81,8 +88,8 @@ void loop() {
   int joy_val_right = analogRead(joystick_right_pin);
   joy_val_right = joy_val_right>>2; // the joystick value is [0..1023] and I want to convert it to 8bit
 
-  int photo_value_left = analogRead(photo_left_pin);
-  int photo_value_right = analogRead(photo_right_pin);
+  photo_value_left_raw = analogRead(photo_left_pin);
+  photo_value_right_raw = analogRead(photo_right_pin);
     
   int pwmValueLeftSym = 128;
   int pwmValueRightSym = 128;
@@ -95,11 +102,10 @@ void loop() {
   Serial.write(joy_val_left);
   Serial.write(joy_val_right); 
   Serial.write((joy_val_left + joy_val_right)%256); // this is the checksum
-  //Serial.println(joy_val_right);// for debugging purposes in serial monitor mode
   if (Serial.available() > 0) {
     dist_ref = (int)(FILTER_CST*dist_ref + (1-FILTER_CST)* Serial.read());
   } else {
-    dist_ref = 0.99*dist_ref;// FIXME tune this param
+    //dist_ref = 0.99*dist_ref;// FIXME tune this param
   }
 
   error_left = dist_ref - sensor2dist(photo_value_left_raw, PHOTO_MIN_LEFT, PHOTO_MAX_LEFT);
@@ -126,9 +132,19 @@ void loop() {
 
   analogWrite(motorPinLowGain, pwmValueLeftSym);//pwmValueLeftSym
   analogWrite(motorPinHighGain, pwmValueRightSym); //pwmValueRightSym
+
+  if (dist_ref < MIN_DIST_THRESH) { // set motor command and integral part to zero, if reference is 0
+    analogWrite(motorPinLowGain, 128);//pwmValueLeftSym
+    analogWrite(motorPinHighGain, 128); //pwmValueRightSym
+    sum_error_left = 0;
+    sum_error_right = 0;
+  }
+
+  //Serial.println(sensor2dist(photo_value_right_raw, PHOTO_MIN_RIGHT, PHOTO_MAX_RIGHT));// for debugging purposes in serial monitor mode
  
   while ((micros() - TIME_BEGIN) < TIME_CYCLE) {  } // do nothing until we reach the time step of TIME_CYCLE
-  digitalWrite(testPin, LOW); //instructions in between take roughly 640 microseconds
+  digitalWrite(testPin, LOW); 
+  analogWrite(controlPin, dist_ref*0.1417); // dist_ref
 }
 
 int sensor2dist(int sensor_value, int min_val, int max_val) {
