4eee5d32f4d9e80dbba49c284fc3b067a3356233
Oechslin Roman <oechslin@winter.ski>
2018-07-03 17:16:58 +0900

    prepare for testing PID on robot

--- Arduino - motor control/arduino_motor_control/arduino_motor_control/arduino_motor_control.ino
+++ Arduino - motor control/arduino_motor_control/arduino_motor_control/arduino_motor_control.ino
@@ -3,6 +3,15 @@
   Robot controller with haptic feedback
   2018/04/06  Tokyo
 */
+#define FASTADC 1
+
+// defines for setting and clearing register bits
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
 
 #define joystick_left_pin A0
 #define photo_left_pin A2
@@ -18,18 +27,27 @@ int OUTPUT_PER_VOLT = 255/5; // the arduino can output 5V max
 int MOTOR_MAX_VOLTAGE = 20; // change this according to motor
 int LEFT_HAND_AMPLIFIER_GAIN = 10;
 int RIGHT_HAND_AMPLIFIER_GAIN = 10;
-bool USE_JOYSTICK_DIRECT_LINK = false;
+const int OUTPUT_RANGE_L = 2 * MOTOR_MAX_VOLTAGE * OUTPUT_PER_VOLT / LEFT_HAND_AMPLIFIER_GAIN;
+const int OUTPUT_RANGE_R = 2 * MOTOR_MAX_VOLTAGE * OUTPUT_PER_VOLT / RIGHT_HAND_AMPLIFIER_GAIN;
 
-int error_left = 0; 
-int last_error_left = 0;
-int sum_error_left = 0;
-int error_right = 0; 
-int last_error_right = 0;
-int sum_error_right = 0;
-const int PHOTO_MIN_LEFT = 514;//500;//0;//514;
-const int PHOTO_MAX_LEFT = 787;//770;//1023;//787;
-const int PHOTO_MIN_RIGHT = 678;//680;//0;//678;
-const int PHOTO_MAX_RIGHT = 773;//770;//1023;//773;
+float error_left = 0;
+//float last_error_left = 0;
+float sum_error_left = 0;
+float error_right = 0;
+//float last_error_right = 0;
+float sum_error_right = 0;
+float dT = 0.001;
+float filter_coeff = 0.05;
+float error_left_last = 0;
+float error_right_last = 0;
+float numerator_l_last_f = 0;
+float numerator_r_last_f = 0;
+
+const int PHOTO_MIN_LEFT = 640;//650;
+const int PHOTO_MAX_LEFT = 840;//830;
+const int PHOTO_MIN_RIGHT = 700;//700;
+const int PHOTO_MAX_RIGHT = 880;//870;
+const int MAX_DISPLACEMENT_UM = 1800; // [um], has been measured
 
 unsigned long TIME_BEGIN = 0;
 unsigned long TIME_NOW = 0;
@@ -37,9 +55,10 @@ unsigned long TIME_CYCLE = 1000; // this is the time_step in [microseconds] that
 
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(250000);
   pinMode(motorPinLowGain, OUTPUT);
   pinMode(motorPinHighGain, OUTPUT);
+  
   pinMode(joystick_left_pin, INPUT);
   pinMode(photo_left_pin, INPUT);
   pinMode(joystick_right_pin, INPUT);
@@ -47,6 +66,12 @@ void setup() {
   pinMode(testPin, OUTPUT);
 
   TCCR2B = TCCR2B & B11111000 | B00000001; // for PWM frequency of 31372.55 Hz
+  #if FASTADC
+  // set prescale to 16
+  sbi(ADCSRA, ADPS2) ;
+  cbi(ADCSRA, ADPS1) ;
+  cbi(ADCSRA, ADPS0) ;
+  #endif
 }
 
 void loop() {
@@ -58,53 +83,40 @@ void loop() {
   int joy_val_right = analogRead(joystick_right_pin);
   joy_val_right = joy_val_right>>2; // the joystick value is [0..1023] and I want to convert it to 8bit
 
-  int pwmValueLeft = 0;
-  int pwmValueRight = 0;
+  int photo_value_left = analogRead(photo_left_pin);
+  int photo_value_right = analogRead(photo_right_pin);
+    
   int pwmValueLeftSym = 128;
   int pwmValueRightSym = 128;
   
-  if (USE_JOYSTICK_DIRECT_LINK) {
-    pwmValueLeft = output2pwm_sym((joy_val_left-127)>0?joy_val_left:0, LEFT_HAND_AMPLIFIER_GAIN);
-    pwmValueRight = output2pwm_sym((joy_val_right-127)>0?joy_val_right:0, RIGHT_HAND_AMPLIFIER_GAIN);
+
+  float k_p = 0.2; // k_p = 1; and k_i = 0.01; works as well
+  float k_i = 0.0;
+  float k_d = 0.0;
+
+  //Writing to Processing file
+  Serial.write(joy_val_left);
+  Serial.write(joy_val_right); 
+  Serial.write((joy_val_left + joy_val_right)%256); // this is the checksum
+  //Serial.println(joy_val_right);// for debugging purposes in serial monitor mode
+  if (Serial.available() > 0) {
+    dist_ref = (int)(FILTER_CST*dist_ref + (1-FILTER_CST)* Serial.read());
   } else {
-    int photo_value_left = analogRead(photo_left_pin);
-    int photo_value_right = analogRead(photo_right_pin);
-    float k_p = 1.0; // k_p = 1; and k_i = 0.01; works as well
-    float k_i = 0.0;
-    float k_d = 0.0;
-    photo_value_left = limit_value(photo_value_left, PHOTO_MIN_LEFT, PHOTO_MAX_LEFT);
-    photo_value_right = limit_value(photo_value_right, PHOTO_MIN_RIGHT, PHOTO_MAX_RIGHT);
-    // just to be sure that it is not out of expected boundaries
-
-    //Serial.println(joy_val_right);
-    Serial.write(joy_val_left);
-    Serial.write(joy_val_right); 
-    Serial.write((joy_val_left + joy_val_right)%256); // this is the checksum
-    //Serial.println(joy_val_right);// for debugging purposes in serial monitor mode
-    if (Serial.available() > 0) {
-      dist_ref = (int)(FILTER_CST*dist_ref + (1-FILTER_CST)* Serial.read());
-    } else {
-      dist_ref = 0.99*dist_ref;// FIXME tune this param
-    }
-    error_left = dist_ref - map_to_255(photo_value_left, PHOTO_MIN_LEFT, PHOTO_MAX_LEFT);
-    error_right = dist_ref - map_to_255(photo_value_right, PHOTO_MIN_RIGHT, PHOTO_MAX_RIGHT);
-        
-    int motor_output_left = k_p * error_left + k_i * sum_error_left + k_d * (error_left - last_error_left);
-    int motor_output_right =  k_p * error_right + k_i * sum_error_right + k_d * (error_right - last_error_right);//FIXME need two different control parameters?
-    sum_error_left = error_left + sum_error_left;
-    last_error_left = error_left;
-    sum_error_right = error_right + sum_error_right;
-    last_error_right = error_right;
-    // feedbackLeft
-    //pwmValueLeft = feedback2pwm(motor_output_left, LEFT_HAND_AMPLIFIER_GAIN); // TODO CHANGETHIS should be motoroutput
-    // feedbackRight
-    //pwmValueRight = feedback2pwm(motor_output_right, RIGHT_HAND_AMPLIFIER_GAIN); // TODO CHANGETHIS should be motoroutput
-    // symmetric feedbackLeft, (allowing for negative motor values)
-    pwmValueLeftSym = output2pwm_sym(motor_output_left, LEFT_HAND_AMPLIFIER_GAIN); //motor_output_left
-    // symmetric feedbackRight, (allowing for negative motor values)
-    pwmValueRightSym = output2pwm_sym(motor_output_right, RIGHT_HAND_AMPLIFIER_GAIN);
-
+    dist_ref = 0.99*dist_ref;// FIXME tune this param
   }
+  error_left = dist_ref - sensor2dist(photo_value_left_raw, PHOTO_MIN_LEFT, PHOTO_MAX_LEFT);
+  error_right = dist_ref - sensor2dist(photo_value_right_raw, PHOTO_MIN_RIGHT, PHOTO_MAX_RIGHT);
+      
+  int motor_output_left = k_p * error_left + k_i * sum_error_left * dt + k_d * (error_left - last_error_left);
+  int motor_output_right =  k_p * error_right + k_i * sum_error_right * dt + k_d * (error_right - last_error_right);//FIXME need two different control parameters?
+  sum_error_left = error_left + sum_error_left;
+  last_error_left = error_left;
+  sum_error_right = error_right + sum_error_right;
+  last_error_right = error_right;
+  // symmetric feedback left and right, (allowing for negative motor values)
+  pwmValueLeftSym = pid2pwm_sym(des_mot_volt_left, OUTPUT_RANGE_L);
+  pwmValueRightSym = pid2pwm_sym(des_mot_volt_right, OUTPUT_RANGE_R);
+
   analogWrite(motorPinLowGain, pwmValueLeftSym);//pwmValueLeftSym
   analogWrite(motorPinHighGain, pwmValueRightSym); //pwmValueRightSym
  
@@ -112,12 +124,11 @@ void loop() {
   digitalWrite(testPin, LOW); //instructions in between take roughly 640 microseconds
 }
 
-int map_to_255(int receptor_value, int min_val, int max_val) {
-  // this function maps the measured value to the feedback range [0..255]
-  int converted = (receptor_value - min_val) * 51 / (max_val - min_val) * 5;
-  converted = 255 - converted;
-  return converted;
+int sensor2dist(int sensor_value, int min_val, int max_val) {
+  // maps the measured value to the distance (assumed linearity) in micrometers
+  return MAX_DISPLACEMENT_UM - (sensor_value - min_val) * (MAX_DISPLACEMENT_UM /15) / (max_val - min_val) * 15;
 }
+
 
 int limit_value(int value, int lower, int upper) {
   if (value < lower) {
@@ -128,14 +139,8 @@ int limit_value(int value, int lower, int upper) {
   return value;
 }
 
-int output2pwm_sym(int output, float gain) {
-  // This function converts the reference symmetrically into a PWM where 0 is full power in one direction and 255 is full power in the other
-  // Due to the non exactness of the sensor MIN and MAX values, this still results in some asymmetries however
-  // conversion of feedback to pwm[0..255]:
-  
-  // limit to MAX_VOLTAGE
-  int limit_val = 2 * MOTOR_MAX_VOLTAGE * OUTPUT_PER_VOLT / gain;
-  float pwm = (float) output / 16 * limit_val / (32) + limit_val / 2;
-  return limit_value((int) pwm, 0, limit_val);
+int pid2pwm_sym(int des_voltage, int range) {
+  // converts motor voltage to pwm output where 128-range/2 is full power in one direction and 128+range/2 is 
+  // full power in the other
+  return limit_value(des_voltage, -range/2, range/2) + 128;
 }
-
