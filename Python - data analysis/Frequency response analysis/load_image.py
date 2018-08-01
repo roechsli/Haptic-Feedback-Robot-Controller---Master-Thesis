d8ba7b6c6cde03ab86c6018e3759979d5e5ed9ff
Oechslin Roman <oechslin@winter.ski>
2018-08-01 11:03:44 +0900

    moved data files into data

--- Python - data analysis/Frequency response analysis/load_image.py
+++ Python - data analysis/Frequency response analysis/load_image.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import pickle
 
 #matplotlib notebook
-
-with open('figs/f2/2_raw.pkl','rb') as fid:
+directory = "C:/Users/Oechslin/Documents/Haptic_Controller_Code/Python - data analysis/Frequency response analysis/tmp_ardu_ref_plots/figs/"
+with open(directory + '_all_in_one_raw.pkl','rb') as fid:
     ax = pickle.load(fid)
 plt.show()
